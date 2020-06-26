7b5656c7a2e311c40506f852953200e91b0fd5a2
16Yongjin <yongjin0802@gmail.com>
2020-06-26 21:22:56 +0900

    main view menu selection

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -2,11 +2,12 @@
 div.d-flex.view.fill-height
   v-navigation-drawer.main-nav(permanent fixed)
     v-list(dense nav)
-      v-list-item(v-for='menu in menus' :key='menu.title' link @click='selectedComponent = menu.component')
-        v-list-item-icon
-          v-icon {{ menu.icon }}
-        v-list-item-content
-          v-list-item-title {{ menu.title }}
+      v-list-item-group(v-model='selectedMenuIndex')
+        v-list-item(v-for='menu in menus' :key='menu.title' link)
+          v-list-item-icon
+            v-icon {{ menu.icon }}
+          v-list-item-content
+            v-list-item-title {{ menu.title }}
 
   components(:is='selectedComponent')
 </template>
@@ -18,7 +19,11 @@ import Search from '@/views/main/Search.vue'
 
 @Component({ components: { RecentlyViewed, Search } })
 export default class MainView extends Vue {
-  selectedComponent = 'RecentlyViewed'
+  selectedMenuIndex = 0
+
+  get selectedComponent() {
+    return this.menus[this.selectedMenuIndex].component
+  }
 
   menus = [
     {
