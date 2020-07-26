dd023eca2d06e1baf493ae2f8240acc487c6c8b6
16Yongjin <yongjin0802@gmail.com>
2020-07-26 21:38:13 +0900

    modify ProjectBar Home Button color

--- src/views/common/ProjectBar.vue
+++ src/views/common/ProjectBar.vue
@@ -1,8 +1,8 @@
 <template lang="pug">
 v-app-bar.project-toolbar(fixed dark elevation='0' height='56px')
   v-btn-toggle.h100(borderless mandatory tile)
-    v-btn.h100(text to="/" :class="isActive ? 'white' : 'black'" )
-      v-icon(:color="isActive ? 'black' : 'white'") mdi-home
+    v-btn.h100(text to="/" )
+      v-icon mdi-home
     v-btn.h100(text v-for='project, i in projects' :key='project.info.id' :to="toUrl(project)")
       span.project-name {{ project.info.name }}
       v-btn.ml-2(icon small)
